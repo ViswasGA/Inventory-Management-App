590dde696425defb0058a15bfd8bab009257ec61
ViswasGA <125551587+ViswasGA@users.noreply.github.com>
2023-07-13 16:17:54 +0530

    Add files via upload

---
+++ Day2/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa"
+import "../Styles/NavbarStyles.css";
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {setClick(!click);
+  }
+
+  return (
+    <div className="header">
+      < Link to="/"> 
+        {/* <h1>Sign Up</h1>      */}
+      </Link>
+      <ul className={click? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/Login"><h1>Login</h1></Link>
+        </li>
+        <li>
+          <Link to="/Register"><h1>Register</h1></Link>
+        </li>
+        <li>
+          <Link to="/"><h1>Home</h1></Link>
+        </li>
+
+        <li>
+          <Link to="/About"><h1>About</h1></Link>
+        </li>
+        <li>
+          <Link to="/Orders"><h1>Orders</h1></Link>
+        </li>
+        <li>
+          <Link to="/Stock"><h1>Stock</h1></Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{ color: "white" }} />) : (<FaBars size={20} style={{ color: "white" }} />)}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
